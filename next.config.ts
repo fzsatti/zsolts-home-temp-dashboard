3f1637104b9865cf7dcb20bab9689e1a313a10c5
Zsolt <zsolt.fodor@endava.com>
2025-09-19 15:12:59 +0300

    Temp dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+const repo = "zsolts-home-temp-dashboard";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",                  // generate static HTML in /out
+    images: { unoptimized: true },     // next/image without loader
+    trailingSlash: true,               // GH Pages serves /about/index.html nicely
+    // For project pages (https://username.github.io/<repo>)
+    basePath: isProd ? `/${repo}` : "",
+    assetPrefix: isProd ? `/${repo}/` : "",
 };
 
 export default nextConfig;
