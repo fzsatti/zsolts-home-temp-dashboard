67c1b066ac7d2102f4a1ec5efff0acbcabd43a0d
Zsolt <zsolt.fodor@endava.com>
2025-09-19 16:52:51 +0300

    Temp dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "github-pages";
 const repo = "zsolts-home-temp-dashboard";
 
 const nextConfig: NextConfig = {
